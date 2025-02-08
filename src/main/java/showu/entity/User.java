58742ca7ac57c79a814c65de1e67d3f1094b77bb
HIRedrain <redrain@yu.ac.kr>
2025-02-08 21:20:58 +0900

    feat - User : of() 구현

--- src/main/java/showu/entity/User.java
+++ src/main/java/showu/entity/User.java
@@ -1,14 +1,27 @@
+/* User.java
+ * showU Service - 자랑
+ * 게시 글 유형 관련 entity
+ * 작성자 : lion4 (김예린, 배희창, 이홍비, 전익주, 채혜송)
+ * 최종 수정 날짜 : 2025.02.08
+ *
+ * ========================================================
+ * 프로그램 수정 / 보완 이력
+ * ========================================================
+ * 작업자       날짜       수정 / 보완 내용
+ * ========================================================
+ * 이홍비    2025.02.08    최초 작성 : DB 설계 기반 entity 작성
+ * 이홍비    2025.02.08    of() 추가
+ * ========================================================
+ */
+
+
 package showu.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import showu.entity.constant.UserRole;
 
 @Getter
 @Setter
@@ -26,7 +39,23 @@ public class User {
     @Column(nullable = false)
     private String userPw;
 
-    @Column(nullable = false, length = 20)
-    private String userRole;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+
+    // 생성자
+    private User(String userId, String userPw) {
+
+        // 초기화
+        this.userId = userId;
+        this.userPw = userPw;
+        this.userRole = UserRole.MEMBER;// default value = member
+    }
+
+    // static factory method - User 객체 생성
+    public static User of(String userId, String userPw) {
+        return new User(userId, userPw);
+    }
 }
 
