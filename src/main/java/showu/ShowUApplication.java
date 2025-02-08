bf5be0ea7df6d33603f34401291b046e4a7dc066
HIRedrain <redrain@yu.ac.kr>
2025-02-08 14:26:46 +0900

    프로젝트 생성

---
+++ src/main/java/showu/ShowUApplication.java
@@ -0,0 +1,13 @@
+package showu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowUApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowUApplication.class, args);
+    }
+
+}
---
+++ src/test/java/showu/ShowUApplicationTests.java
@@ -0,0 +1,13 @@
+package showu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowUApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
