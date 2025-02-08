a78172a8788f4cd7dab5394f386988a9dc21b967
madsensss <madsenss@naver.com>
2025-02-08 20:18:41 +0900

    .

--- src/main/java/showu/controller/PostController.java
+++ src/main/java/showu/controller/PostController.java
@@ -3,6 +3,7 @@ package showu.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -26,6 +27,7 @@ import showu.service.PostService;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/post")
 public class PostController {
 	private final PostService postService;
 	
