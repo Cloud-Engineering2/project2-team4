208865f178b3e4cae0a61b446351f0dee40dd638
LeeHongBi <97448513+HIRedrain@users.noreply.github.com>
2025-02-08 23:52:36 +0900

    feat - Post : of() 추가

--- src/main/java/showu/entity/Post.java
+++ src/main/java/showu/entity/Post.java
@@ -40,38 +40,39 @@ import lombok.Setter;
 @Entity
 @Table(name = "posts")
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long pid;
+    private Long pid; // 게시 글 고유 id
 
     @ManyToOne
     @JoinColumn(name = "uid", nullable = false)
-    private User user;
+    private User user; // 작성자
 
     @ManyToOne
     @JoinColumn(name = "cid", nullable = false)
-    private Category category;
+    private Category category; // 게시 글 유형
 
     @Column(nullable = false, length = 50)
-    private String title;
+    private String title; // 제목
 
     @Column(nullable = false, columnDefinition = "TEXT")
-    private String content;
+    private String content; // 내용
 
     @Column(columnDefinition = "TEXT")
-    private String link;
+    private String link; // 링크
 
     @Column(columnDefinition = "TEXT")
-    private String imageUrl;
+    private String imageUrl; // 파일 링크
 
     @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
-    private int plike;
+    private int plike; // 좋아요 수
 
     @CreationTimestamp
-    private LocalDateTime createdDate;
+    private LocalDateTime createdDate; // 최초 작성 일자
 
     @UpdateTimestamp
-    private LocalDateTime modifiedDate;
+    private LocalDateTime modifiedDate; // 최종 수정 일자
 
 
     // 생성자
@@ -87,9 +88,28 @@ public class Post {
         this.plike = 0;
     }
 
+    public Post(User user, Category category, String title, String content, String link, String imageUrl, int plike, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+
+        // 초기화
+        this.user = user;
+        this.category = category;
+        this.title = title;
+        this.content = content;
+        this.link = link;
+        this.imageUrl = imageUrl;
+        this.plike = plike;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
+
+
     // static factory method - Post 객체 생성
     public static Post of(User user, Category category, String title, String content, String link, String imageUrl) {
         return new Post(user, category, title, content, link, imageUrl);
+    }
+
+    public static Post of(User user, Category category, String title, String content, String link, String imageUrl, int plike, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+        return new Post(user, category, title, content, link, imageUrl, plike, createdDate, modifiedDate);
     }
 
 }
