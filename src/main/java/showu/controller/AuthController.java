18633dc6f832fa6b5276eae968e9c444ec1a74e3
HIRedrain <redrain@yu.ac.kr>
2025-02-10 13:58:53 +0900

    feat -AuthController : logout() 구현 fix - AuthController : login() - try-catch 제거

--- src/main/java/showu/controller/AuthController.java
+++ src/main/java/showu/controller/AuthController.java
@@ -2,7 +2,7 @@
  * showU Service - 자랑
  * 로그인 api 처리 컨트롤러
  * 작성자 : lion4 (김예린, 배희창, 이홍비, 전익주, 채혜송)
- * 최종 수정 날짜 : 2025.02.08
+ * 최종 수정 날짜 : 2025.02.10
  *
  * ========================================================
  * 프로그램 수정 / 보완 이력
@@ -10,13 +10,17 @@
  * 작업자       날짜       수정 / 보완 내용
  * ========================================================
  * 배희창   2025.02.08    최초 작성 : AuthController 작성
- * 채혜송   2025.02.09    회원가입 수정 및 탈퇴 API 추가
+ * 채혜송   2025.02.09    회원 가입 수정 및 탈퇴 API 추가
+ * 배희창   2025.02.09    login() - 토큰 로컬 스토리지 저장 추가
+ * 이홍비   2025.02.10    login() - try-catch 제거 // logout() 구현
  * ========================================================
  */
 package showu.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +31,11 @@ import showu.dto.LoginRequestDTO;
 import showu.dto.UserDTO;
 import showu.entity.User;
 import showu.service.UserService;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -59,7 +68,38 @@ public class AuthController {
 
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody LoginRequestDTO loginRequest) {
+
+		System.out.println("🔥 로그인 도전! - Controller : " + loginRequest.getUserId() + ", " + loginRequest.getUserPw());
+
 		String token = userService.login(loginRequest);
-		return ResponseEntity.ok().body(token);
+
+		System.out.println("✅ 로그인 성공! - Controller : " + token);
+
+		// 토큰 주입
+		Map<String, String> response = new HashMap<>();
+		response.put("token", token);
+
+		return ResponseEntity.ok(response);
+	}
+
+	@PostMapping("/logout")
+	public ResponseEntity<?> logout(HttpServletRequest request) {
+
+		System.out.println("🔥 로그아웃 도전! - Controller");
+
+		String token = request.getHeader("Authorization"); // 토큰 get
+		if (token != null && !token.isEmpty()) {
+			// 토큰 존재 o
+			System.out.println("🚪 로그아웃 성공! - Controller : " + token);
+
+			return ResponseEntity.ok(Collections.singletonMap("message", "로그아웃 되었습니다."));
+		} else {
+			// 토큰 못 받음
+			System.out.println("❌ 로그아웃 실패! - Controller : " + token);
+
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(Collections.singletonMap("message", "토큰이 제공되지 않았습니다."));
+		}
+
 	}
 }
