98ff834e3de393293cedaa34e7898cf87abc1ffc
leGit-y <99312658+leGit-y@users.noreply.github.com>
2025-02-11 02:20:00 +0900

    feat: post detail page

--- src/main/java/showu/security/SecurityConfig.java
+++ src/main/java/showu/security/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/js/**", "/images/**", "/favicon.ico",  "/postput").permitAll() // 정적 자원 허용
-                        .requestMatchers("/", "/login", "/signup", "/posttest", "/postget", "/postdelete", "/postput").permitAll() // 얘는 정적 페이지 로그인 없이 가능하게 함
+                        .requestMatchers("/", "/login", "/signup", "/posttest", "/postget", "/postdelete", "/postput", "/postdetail", "/postone", "/tmp").permitAll() // 얘는 정적 페이지 로그인 없이 가능하게 함
                         .requestMatchers("/api/login/**", "/api/signup/**").permitAll() // 얘가 있어야 권한 없이 로그인이랑 회원 가입 가능
                         // .requestMatchers("/api/**").permitAll() // 개발 중이라 전부 열어 놨는데 나중에 무조건 지워야 함. 안 지우면 클나요
                         .requestMatchers("/test").authenticated() // test 용 - 추후 삭제
