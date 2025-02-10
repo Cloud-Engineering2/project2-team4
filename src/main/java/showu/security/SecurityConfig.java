d0e3540a42b6c08aebd528837c4ad196bfbba69d
madsensss <madsenss@naver.com>
2025-02-10 22:55:56 +0900

    securityconfig 수정

--- src/main/java/showu/security/SecurityConfig.java
+++ src/main/java/showu/security/SecurityConfig.java
@@ -24,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -75,4 +76,3 @@ public class SecurityConfig {
         return http.build();
     }
 }
-
